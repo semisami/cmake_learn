e65a32a910413cf6f1a536862ed00c6253500c5e
hossein samanipoor <hosseinsamanipuor@gmail.com>
2024-05-30 01:58:54 +0330

    Ep_2 code

---
+++ ep_2/source/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b){
+    return a + b;
+}
+
+int main(){
+
+    std::cout << "C++ 20" << std::endl;
+    std::cout << "Sum: " << add(3, 5) << std::endl;
+
+    return 0;
+
+}
